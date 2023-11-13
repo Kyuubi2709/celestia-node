0551c33cb3d988f12c7f743470409873e7e02d9a
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-11-13 21:15:20 +0100

    Fix permissions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,9 +6,13 @@ if [ "$1" = 'celestia' ]; then
     echo "Initializing Celestia Node with command:"
 
     if [[ -n "$NODE_STORE" ]]; then
+        chmod 700 "${NODE_STORE}"
+        chown -R celestia:celestia "${NODE_STORE}"
         echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
         celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
     else
+        chmod 700 /home/celestia
+        chown -R celestia:celestia /home/celestia
         echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
         celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
     fi
